8b04c578f7245b7497849d6cb348c4d0304a992d
Dina Dehbi <dinadh012@gmail.com>
2024-11-29 01:10:01 +0100

    tuto7.1

--- app/src/main/java/com/example/kotlin_advance_concept/tuto7-1.kt
+++ app/src/main/java/com/example/kotlin_advance_concept/tuto7-1.kt
@@ -1,24 +1,60 @@
-package com.example.kotlin_advance_concept
+package com.example.tuto1
 
-// Enum class pour la difficulté
-enum class Difficulty {
-    EASY, MEDIUM, HARD
-}
-
-// Classe générique Question
-class Question<T>(
+data class Question<T>(
     val questionText: String,
     val answer: T,
     val difficulty: Difficulty
 )
 
-fun main() {
+enum class Difficulty{
+    EASY, MEDIUM, HARD
+}
+class Quiz:ProgressPrintable {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
-    println("${question1.questionText} (Difficulty: ${question1.difficulty}) Answer: ${question1.answer}")
-
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
-    println("${question2.questionText} (Difficulty: ${question2.difficulty}) Answer: ${question2.answer}")
-
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
-    println("${question3.questionText} (Difficulty: ${question3.difficulty}) Answer: ${question3.answer}")
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+    override val progressText: String
+        get()= "$answered  questions answered from $total "
+    override fun printProgressBar(){
+        repeat(Quiz.answered) { print("▓") }
+        repeat(Quiz.total - Quiz.answered) { print("▒") }
+        println()
+        println(progressText)
+    }
+    fun printQuiz() {
+        question1.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+        question2.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+        question3.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+    }
+}
+
+
+interface ProgressPrintable{
+    val progressText:String
+    fun printProgressBar()
+}
+
+fun main() {
+    Quiz().apply {
+        printQuiz()
+    }
 }
